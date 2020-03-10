bde0b8f8a7bf9d9dcfda7ad1f5e19d9f3b057a6c
mook <keettamook_k@cmu.ac.th>
2020-03-10 16:15:27 +0700

    finnish

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,17 +1,36 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(Rect,Rect);
+double overlap(Rect a,Rect b){
+	double dx,dy;
+	double dx1 = a.x+a.w;
+	double dx2 = b.x+b.w;
+	dx = min(dx1,dx2) - max(a.x,b.x);
+
+	double dy1 = a.y-a.h;
+	double dy2 = b.y-b.h;
+	dy = min(a.y,b.y) - max(dy1,dy2);
+
+	if ((dx*dy) >=0){
+		return dx*dy;
+	}else return 0;
+	
+
+}
 
 int main(){
+	Rect a,b;
 	cout << "Please input Rect 1 (x y w h): ";
+	cin >> a.x >> a.y >> a.w >> a.h;
 	cout << "Please input Rect 2 (x y w h): ";
+	cin >> b.x >> b.y >> b.w >> b.h;
 	
 	
-	cout << "Overlap area = ";	
+	cout << "Overlap area = " << overlap(a,b);	
 	return 0;
 }
